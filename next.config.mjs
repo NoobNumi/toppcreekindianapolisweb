20522bba0c792753bce5e9ff0e461c1e32c4d334
Niña Gillian Villamin <124445105+NoobNumi@users.noreply.github.com>
2025-03-25 21:17:09 +0800

    Update dependencies in package.json and enhance next.config.mjs with bundle analyzer integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
+import bundleAnalyzer from '@next/bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   experimental: {
     optimizeCss: true,
@@ -7,13 +13,13 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     domains: ["a0.muscache.com"],
-  },  
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push("leaflet");
     }
     return config;
   },
-};
+});
 
 export default nextConfig;
