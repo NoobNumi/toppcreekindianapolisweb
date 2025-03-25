e4fc9213388c5b64988afb15d576c18192a660d8
Niña Gillian Villamin <124445105+NoobNumi@users.noreply.github.com>
2025-03-25 21:09:44 +0800

    Refactor next.config.mjs: remove bundle analyzer integration, enable SWC minification, and optimize CSS settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,20 @@
 /** @type {import('next').NextConfig} */
-import bundleAnalyzer from '@next/bundle-analyzer';
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-const nextConfig = withBundleAnalyzer({
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     optimizeCss: true,
   },
   images: {
     formats: ["image/avif", "image/webp"],
-    domains: ["a0.muscache.com"], 
-  },
+    domains: ["a0.muscache.com"],
+  },  
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push("leaflet");
     }
     return config;
   },
-});
+};
 
 export default nextConfig;
